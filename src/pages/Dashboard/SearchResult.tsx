fbcd28bdcd8b197d355a34ba5143684f25d167fc
aayodejii <e.manuel_07@yahoo.com>
2023-06-07 21:53:03 +0100

    comments resolution

--- src/pages/Dashboard/SearchResult.tsx
+++ src/pages/Dashboard/SearchResult.tsx
@@ -17,9 +17,9 @@ const SearchResult = () => {
           <span>Phone number</span>
           <span>Date</span>
         </div>
-        <div className="w-full relative my-3 h-[40px]">
+        <div className="w-full relative my-3 h-[60px]">
           <div
-            className="absolute w-full bg-white z-10 grid items-center p-3 text-[14px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
+            className="absolute w-full bg-white z-10 grid items-center p-3 text-[12px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
             style={{ gridTemplateColumns: "15% 20% 25% 20% 20%" }}
           >
             <img src="/user.png" alt="" className="w-8" />
@@ -30,11 +30,48 @@ const SearchResult = () => {
           </div>
           <div className="absolute top-1 w-full grid items-center p-3 text-[14px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"></div>
         </div>
-
-        <div className="w-full relative top-5 my-3 h-[40px] overflow-y-scroll">
+        <div className="w-full relative my-3 h-[60px]">
           <div
-            className="absolute w-full bg-white z-10 grid items-center p-3 text-[14px] text-[#453953] 
-            border border-[#453953] h-[54px] rounded-xl overflow-x-scroll"
+            className="absolute w-full bg-white z-10 grid items-center p-3 text-[12px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
+            style={{ gridTemplateColumns: "15% 20% 25% 20% 20%" }}
+          >
+            <img src="/user.png" alt="" className="w-8" />
+            <span>Tyme Investment</span>
+            <span>jamesike@yahoo.com</span>
+            <span>+1(645) 555-3423</span>
+            <span>25/02/2023</span>
+          </div>
+          <div className="absolute top-1 w-full grid items-center p-3 text-[14px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"></div>
+        </div>
+        <div className="w-full relative my-3 h-[60px]">
+          <div
+            className="absolute w-full bg-white z-10 grid items-center p-3 text-[12px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
+            style={{ gridTemplateColumns: "15% 20% 25% 20% 20%" }}
+          >
+            <img src="/user.png" alt="" className="w-8" />
+            <span>Tyme Investment</span>
+            <span>jamesike@yahoo.com</span>
+            <span>+1(645) 555-3423</span>
+            <span>25/02/2023</span>
+          </div>
+          <div className="absolute top-1 w-full grid items-center p-3 text-[14px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"></div>
+        </div>
+        <div className="w-full relative my-3 h-[60px]">
+          <div
+            className="absolute w-full bg-white z-10 grid items-center p-3 text-[12px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
+            style={{ gridTemplateColumns: "15% 20% 25% 20% 20%" }}
+          >
+            <img src="/user.png" alt="" className="w-8" />
+            <span>Tyme Investment</span>
+            <span>jamesike@yahoo.com</span>
+            <span>+1(645) 555-3423</span>
+            <span>25/02/2023</span>
+          </div>
+          <div className="absolute top-1 w-full grid items-center p-3 text-[14px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"></div>
+        </div>
+        <div className="w-full relative my-3 h-[60px]">
+          <div
+            className="absolute w-full bg-white z-10 grid items-center p-3 text-[12px] text-[#453953] border border-[#453953] h-[54px] rounded-xl"
             style={{ gridTemplateColumns: "15% 20% 25% 20% 20%" }}
           >
             <img src="/user.png" alt="" className="w-8" />
